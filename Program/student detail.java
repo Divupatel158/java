850eb5497f3ce65ad57eb20e2903e4a1e243fdca
Divupatel158 <146370149+Divupatel158@users.noreply.github.com>
2023-10-04 00:21:02 +0530

    03-10-2023.

---
+++ Program/student detail.java
@@ -0,0 +1,29 @@
+student detail
+
+write a java progrtam to print student name ,student's grade,CGPA and print all the details in single print statements take four student data.
+
+class stusent
+{
+p.s.v.m(string[] args)
+{
+string sname1,sname2,sname3,sname4;
+sname1="vruda";
+sname2="nandani";
+sname3="janvi";
+sname4="payal";
+char g1,g2,g3,g4;
+g1='A';
+g2='B';
+g3='A';
+g4='B';
+double cg1,cg2,cg3,cg4; 
+cg1=5;
+cg2=4.2;
+cg3=4.3;
+cg4=4.5;
+system.out.println(sname1+"gread is "+g1+"CGPA is "+cg1);
+system.out.println(sname2+"gread is "+g2+"CGPA is "+cg2);
+system.out.println(sname3+"gread is "+g3+"CGPA is "+cg3);
+system.out.println(sname4+"gread is "+g4+"CGPA is "+cg4);
+}
+}
