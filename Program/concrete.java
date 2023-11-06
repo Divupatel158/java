ac224ce6234a3e3e22a7b27324ff2e745640e171
Divupatel158 <146370149+Divupatel158@users.noreply.github.com>
2023-11-06 13:06:39 +0530

    06-11-1

---
+++ Program/concrete.java
@@ -0,0 +1,11 @@
+package oop6;
+abstract class leo {//abstract class
+	abstract void dance();//abstract method 
+}
+public class concrete {// concrete class 
+		void fight() {// concrete method
+			System.out.println("inside of concrete class");
+}
+	//Abstract class will allow both abstract methods and concrete methods
+	//Concrete class will not allow abstract methods
+}
